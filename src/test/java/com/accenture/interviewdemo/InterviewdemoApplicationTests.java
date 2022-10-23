6deab728d8c794f150692f86f677fe3e122c94c8
Lovedeep Sangha <lovedeepsangha71@icloud.com>
2022-10-22 20:55:18 -0700

    intial commit

---
+++ src/test/java/com/accenture/interviewdemo/InterviewdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.interviewdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
